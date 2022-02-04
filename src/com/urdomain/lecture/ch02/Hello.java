03ee7e93b6697deba394c610bdb48120b3304ebe
ezen <ezen@DESKTOP-O03MT84.mshome.net>
2022-02-04 15:35:48 +0900

    ㅁㄴㅇㄹ

--- src/com/urdomain/lecture/ch02/Hello.java
+++ src/com/urdomain/lecture/ch02/Hello.java
@@ -5,6 +5,17 @@ public class Hello {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to HELL !!!!!");
+		
+		int a = 1;	// 선언 대입(초기화)
+		int b = 2;
+		
+		int c = 0;
+		
+		c = a + b;
+		
+		System.out.println(c);
+		System.out.println(c);
+		
 	}
-
+	
 }
