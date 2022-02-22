d03d1204efb374d1360a1d85460021cb5e2a39b3
excitingmvr <EZEN@DESKTOP-5STTIAP>
2022-02-22 17:37:50 +0900

    sf

---
+++ src/com/urdomain/lecture/ch06/ThirdCalculator.java
@@ -0,0 +1,13 @@
+package com.urdomain.lecture.ch06;
+
+public class ThirdCalculator {
+	
+	public double areaRectangle(double a) { 
+		return a * a;
+	}
+
+	public double areaRectangle(double a, int b) { 
+		return a * b;
+	}
+	
+}
