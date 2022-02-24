836935d10444a808c596d2069f71cfb86b7d3a87
excitingmvr <EZEN@DESKTOP-5STTIAP>
2022-02-24 17:38:21 +0900

    ㅁㄴㅇㅀㄴㅁㅇ

---
+++ src/com/urdomain/lecture/ch07/Iphone.java
@@ -0,0 +1,23 @@
+package com.urdomain.lecture.ch07;
+
+public class Iphone {
+	
+	private String number = "";
+	
+	
+	public void print(String msg) {
+		System.out.println(msg);
+	}
+	
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	
+
+}
---
+++ src/com/urdomain/lecture/ch07/MyFriendInfo.java
@@ -0,0 +1,24 @@
+package com.urdomain.lecture.ch07;
+
+public class MyFriendInfo {
+	
+	private String name = "";
+	private Integer age = 0;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	
+	
+}
